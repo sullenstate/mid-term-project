e17400388d3c98ce684eb2fa3dea74b116ce80cc
Tim <tklein@outlook.com>
2015-07-14 19:10:56 -0600

    Tuesday Final Commit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // Save page state in local storage
 var pageState = function(){
-	setTimeout(function(){
 		console.log("I be savin' yo data!");
 		localStorage["projectPageState"] = JSON.stringify($('.save-container').html());
-	}, 2000);
 };
 
 $(document).on('ready', function() {
 
 	// Load locally stored page content
-	$('.save-container').html(JSON.parse(localStorage["projectPageState"]));
+	// $('.save-container').html(JSON.parse(localStorage["projectPageState"]));
 
+	$("[data-toggle=popover]").popover();
+	
 	// Top Nav Bar Click Events
 	$('ul.navbar-nav li').on('click', function(){
 		if ($(this).hasClass('active')) {
@@ -20,12 +20,14 @@ $(document).on('ready', function() {
 				$('.navbar-right').children('li.active').removeClass('active');
 				$('.sidebar-menu .container-fluid').fadeOut(100);
 				$('.sidebar-menu').animate({width: '0%'});
-				$('.main-content').animate({width: '90%', marginLeft: '5%'});
-				pageState();
+				$('.main-content').animate({width: '90%', marginLeft: '5%'}, function(){
+					pageState();
+				});
 			}
 		}
 		else {
 			$(this).addClass('active');
+			$(this).children('.fa-tasks').popover('show');
 			$(this).siblings('li.active').removeClass('active');
 			// Sidebar Hamburger - Deavtivate other Top Nav Elements and Show Sidebar
 			if($(this).children('a').hasClass('fa-bars')) {
@@ -33,8 +35,9 @@ $(document).on('ready', function() {
 				$('.sidebar-menu').animate({width: '15%'}, function(){
 					$('.sidebar-menu .container-fluid').fadeIn(400);
 				});
-				$('.main-content').animate({width: '75%', marginLeft: '20%'});
-				pageState();
+				$('.main-content').animate({width: '75%', marginLeft: '20%'}, function(){
+					pageState();
+				});
 			}
 		}
 	});
@@ -44,24 +47,34 @@ $(document).on('ready', function() {
 		// Handle Sidebar accordions
 		if ($(this).hasClass('active')) {
 			$(this).removeClass('active').children('p').slideUp();
-			pageState();
 		}
 		else	{
 			$(this).addClass('active').children('p').slideDown();
 			$(this).siblings().removeClass('active').children('p').slideUp();
-			pageState();
+		}
+
+		setTimeout(pageState, 1000);
+	});
+
+	$('.sidebar-menu li > p').on('click', function(event){
+		// Handle menu items
+		event.stopPropagation();
+		console.log('Menu Item be Clicked');
+		if ($(this).hasClass('add')) {
+			$('table.tasks.active').toggleClass('active').fadeToggle();
+			$('#projectEntryModal').modal();
 		}
 	});
 
 	// Project Click Events
-	$('table.projects tr.project').on('click', function(){
+	$('.main-content').on('click', 'table.projects tr.project', function(){
 		var selectedProjectTasks = $(this).find('table.tasks');
 		var activeProjectTasks = $(this).siblings('tr').find('table.tasks.active');
 
 		activeProjectTasks.toggleClass('active').fadeToggle();
-		selectedProjectTasks.toggleClass('active').fadeToggle();
-		pageState();
-
+		selectedProjectTasks.toggleClass('active').fadeToggle(function(){
+			pageState();
+		});
 	});
 
 	// Task Click Events
@@ -72,7 +85,26 @@ $(document).on('ready', function() {
 		event.stopPropagation();
 		
 		$('#modalName').text(taskName);
-		$('#modalDesc').html('<small>' + taskDesc + '</small>');
-		$('#myModal').modal();
+		$('#modalDesc').html('<small><em>' + taskDesc + '</em></small>');
+		$('#taskDetailModal').modal();
+	});
+
+	// Form Click Events
+	$('.modal-footer .btn-primary').on('click', function(){
+		var projectName = $('#projectName').val();
+		var projectDesc = $('#projectDescription').val();
+		var newProject = $('table.template .project');
+
+		// Populate Project Template
+		newProject.find('h2').first().text(projectName);
+		newProject.find('em').first().text(projectDesc);
+		// Add Project Template Clone to Main Table Body
+		$('table.projects > tbody').prepend(newProject.clone());
+		// Close Modal and Clear Form Data
+		$('#projectEntryModal').modal('toggle');
+		$('#projectName').val('');
+		$('#projectDescription').val();
+		// Save New Page State
+		setTimeout(pageState, 1000);
 	});
 });
